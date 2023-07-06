e58e6b894d9948fb58f59bc100acead12d8a2009
Ikorian <138433077+Ikorian@users.noreply.github.com>
2023-07-06 11:43:11 +0300

    Update Counter.tsx

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { observer } from 'mobx-react';
 import { makeAutoObservable} from 'mobx';
-
+type Item={ 
+    key: number | string;
+    value: string;
+};
 class CounterStore{
+    items:Item[];
     count: number=0;
     constructor() {
         makeAutoObservable(this)
+        this.items=[
+            {key:1, value: '1'},
+            {key:2, value: '2'},
+            {key:3, value: '3'},
+            {key:4, value: '4'},
+            {key:5, value: '5'},
+            {key:6, value: '6'},
+            {key:7, value: '7'},
+            {key:8, value: '8'},
+            {key:9, value: '9'},
+            {key:10, value: '10'},
+        ];
     }
     increment(){
         this.count++;
